4262dba6c8727443c4196875c43c9a8fb1c53ad6
alonsoguerrero <alonsoguerrero710@gmail.com>
2025-07-24 14:53:55 -0600

    Initial commit

---
+++ src/leet/code/data/structures/linkedlist/singly/RemoveNthNodeFromEndOfList/main.java
@@ -0,0 +1,18 @@
+package leet.code.data.structures.linkedlist.singly.RemoveNthNodeFromEndOfList;
+
+
+public class main {
+
+    public static void main(String[] args) {
+
+
+        ListNode myLinkedList = new ListNode(1);
+        myLinkedList.next = new ListNode(2);
+        myLinkedList.next.next = new ListNode(3);
+
+        myLinkedList.printList();
+
+    }
+
+
+}
